2dddbc782ebbb3194ceb40b193a32ff0b13ec9f8
Huanchen Zhai <hczhai@ucla.edu>
2021-02-05 13:41:32 -0800

    cost flow

--- src/max_flow.hpp
+++ src/max_flow.hpp
@@ -22,6 +22,7 @@
 
 #include <cstdint>
 #include <cstring>
+#include <limits>
 #include <unordered_map>
 #include <vector>
 
@@ -113,5 +114,105 @@ struct FLOW {
                     dist[ri.first] = dist[x] + 1, q[r++] = ri.first;
         }
         return dist[n + 1] != -1;
+    }
+};
+
+// Min Cost Max flow (SAP & SPFA)
+struct CostFLOW {
+    typedef unordered_map<int, pair<int, int>>::const_iterator mci;
+    vector<unordered_map<int, pair<int, int>>> resi;
+    vector<int> dist;
+    vector<int> pre;
+    int n, nfs, inf;
+    CostFLOW(int n) : n(n) {
+        resi.resize(n + 2);
+        dist.resize(n + 2);
+        pre.resize(n + 2);
+        nfs = 0;
+        inf = numeric_limits<int>::max();
+    }
+
+    void MVC_DFS(int i, uint8_t *vis) {
+        vis[i] = 1;
+        for (const auto &ri : resi[i])
+            if (ri.second.first != 0 && !vis[ri.first])
+                MVC_DFS(ri.first, vis);
+    }
+    // Minimum Vertex Cover (bipartite graph)
+    int MVC(int xi, int yi, int xn, int yn, vector<int> &vx, vector<int> &vy) {
+        SAP();
+        vector<uint8_t> vis;
+        vis.reserve(n + 2);
+        memset(vis.data(), 0, (n + 2) * sizeof(uint8_t));
+        MVC_DFS(n, vis.data());
+        vx.reserve(xn);
+        vy.reserve(yn);
+        int cost = 0;
+        for (int i = 0; i < xn; i++)
+            if (resi[n][xi + i].first == 0 && !vis[xi + i])
+                vx.push_back(i), cost += resi[n][xi + i].second;
+        for (int i = 0; i < yn; i++)
+            if (resi[yi + i][n + 1].first == 0 && vis[yi + i])
+                vy.push_back(i), cost += resi[yi + i][n + 1].second;
+        return cost;
+    }
+
+    // Shortest Augmenting Path
+    pair<int, int> SAP() {
+        int flow = 0, cost = 0;
+        for (; SPFA();) {
+            for (int i = n + 1; i != n; i = pre[i]) {
+                resi[pre[i]].at(i).first -= pre[n];
+                if (resi[i].count(pre[i]))
+                    resi[i][pre[i]].first += pre[n];
+                else
+                    resi[i][pre[i]] =
+                        make_pair(pre[n], -resi[pre[i]][i].second);
+            }
+            flow += pre[n];
+            cost += pre[n] * dist[n + 1];
+        }
+        return make_pair(flow, cost);
+    }
+
+    // Shortest Path Faster Algorithm
+    bool SPFA() {
+        int nn = n + 2;
+        vector<int> q;
+        q.reserve(nn + 1);
+        vector<uint8_t> v;
+        v.reserve(nn);
+        int h = 0, l = 1, x;
+        for (int i = 0; i < nn; i++)
+            dist[i] = inf;
+        memset(pre.data(), -1, nn * sizeof(int));
+        memset(v.data(), 0, nn * sizeof(uint8_t));
+        dist[n] = 0, pre[n] = -1;
+        q[0] = n, v[n] = 1;
+        while (l != 0) {
+            x = q[h];
+            v[q[h]] = 0;
+            l--;
+            h = (h + 1) % nn;
+            for (mci ix = resi[x].begin(); ix != resi[x].end(); ix++) {
+                if (ix->second.first == 0)
+                    continue;
+                int i = ix->first, p = ix->second.second;
+                if (dist[x] + p < dist[i]) {
+                    dist[i] = dist[x] + p;
+                    pre[i] = x;
+                    if (!v[i])
+                        v[q[(h + l++) % nn] = i] = 1;
+                }
+            }
+        }
+        if (pre[n + 1] != -1) {
+            int flow = inf;
+            for (int i = n + 1; i != n; i = pre[i])
+                if (resi[pre[i]][i].first < flow)
+                    flow = resi[pre[i]][i].first;
+            pre[n] = flow;
+        }
+        return pre[n + 1] != -1;
     }
 };
