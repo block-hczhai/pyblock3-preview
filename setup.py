f781e92b9888b9bfd11542f00f9cbe895111e57e
Huanchen Zhai <hczhai@ucla.edu>
2021-06-19 22:02:35 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='pyblock3',
+      version='0.1.0',
+      packages=['pyblock3'],
+      license='LICENSE',
+      description='An Efficient python block-sparse tensor and MPS/DMRG Library.',
+      long_description=open('README.md').read(),
+      author='Huanchen Zhai, Yang Gao, and Garnet K.-L. Chan',
+      author_email='hczhai@ucla.edu',
+      url='https://github.com/block-hczhai/pyblock3-preview',
+      install_requires=[
+          "numpy",
+          "numba",
+          "psutil"
+      ]
+      )
